20ceb794ec998c4e54952dfb88e511c0e439de8e
Vedant <acharivedant2@gmail.com>
2025-01-01 21:51:46 +0530

    audio-video-animation

--- audio-video-player/script.js
+++ audio-video-player/script.js
@@ -1,10 +1,21 @@
 let a = window.addEventListener("load", () => {
     let element = document.getElementsByClassName("box")[0]; 
+    let element1 = document.getElementsByClassName("b1")[0]; 
+    let element2 = document.getElementsByClassName("b2")[0]; 
+
     element.style.color = "#130f30";
     element.style.transform = "translateY(-10px)";
+    element1.style.transform = "translateX(-50px)";
+    element2.style.transform = "translateX(50px)";
+
     setTimeout(() => {
         element.style.transform = "translateY(-120px) scale(0.9)"; 
-        element.style.transition = "all 3s cubic-bezier(.05,.31,.19,.62)"; 
+        element.style.transition = "all 3s cubic-bezier(.05,.31,.19,.62)";
+        element1.style.opacity = "100";
+        element2.style.opacity = "100";
+        element1.style.transform = "translateX(10px)";
+        element2.style.transform = "translateX(-10px)";
 
-    }, 3000); 
+    }, 3000);
+
 });
