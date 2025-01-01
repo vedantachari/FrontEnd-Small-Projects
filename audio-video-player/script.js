7e4638d016cbacf9d33fa71cf3932254ebd2808a
Vedant <acharivedant2@gmail.com>
2025-01-01 23:26:45 +0530

    added-hover-effect

--- audio-video-player/script.js
+++ audio-video-player/script.js
@@ -11,11 +11,41 @@ let a = window.addEventListener("load", () => {
     setTimeout(() => {
         element.style.transform = "translateY(-120px) scale(0.9)"; 
         element.style.transition = "all 3s cubic-bezier(.05,.31,.19,.62)";
+        element1.style.transition = "all 3s cubic-bezier(.05,.31,.19,.62)";
+        element2.style.transition = "all 3s cubic-bezier(.05,.31,.19,.62)";
         element1.style.opacity = "100";
         element2.style.opacity = "100";
         element1.style.transform = "translateX(10px)";
         element2.style.transform = "translateX(-10px)";
-
     }, 3000);
 
 });
+
+let b = document.querySelector(".b1")
+b.style.transition = "transform 0.1s ease-in-out";
+b.addEventListener("mouseover", ()=>
+{
+    b.classList.toggle(".b1")
+})
+
+// let b = document.querySelector(".b1");
+
+// b.addEventListener("mouseover", () => {
+//     b.classList.toggle("hovered");
+// });
+b.addEventListener("mouseout", ()=>
+{
+    b.style.transform = "scale(1) translateX(10px)";
+})
+
+let c = document.querySelector(".b2")
+c.style.transition = "transform 0.1s ease-in-out";
+c.addEventListener("mouseover", ()=>
+{
+    c.style.transform = "scale(1.1)";
+})
+
+c.addEventListener("mouseout", ()=>
+{
+    c.style.transform = "scale(1) translateX(-10px)";
+})
